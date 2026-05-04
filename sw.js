064c39b166acf95880745765bcaff727f9219254
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-05-03 21:56:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('web2apk').then(c => c.addAll(['/']))));
+self.addEventListener('install', e => e.waitUntil(caches.open('convertapk').then(c => c.addAll(['/']))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
